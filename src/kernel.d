77a2978549b03bbe6db9ed2ad07f68a15edd6366
Jonathan Voss <jv96@mahurangi.school.nz>
2022-03-26 10:42:52 +1300

    Fixing `.gitignore` to remove to build files

--- src/kernel.d
+++
@@ -1,5 +0,0 @@
-src/kernel.o: src/kernel.c \
- /Users/jonathanvoss/opt/cross/lib/gcc/i686-elf/12.0.1/include/stdbool.h \
- /Users/jonathanvoss/opt/cross/lib/gcc/i686-elf/12.0.1/include/stddef.h \
- /Users/jonathanvoss/opt/cross/lib/gcc/i686-elf/12.0.1/include/stdint.h \
- /Users/jonathanvoss/opt/cross/lib/gcc/i686-elf/12.0.1/include/stdint-gcc.h
