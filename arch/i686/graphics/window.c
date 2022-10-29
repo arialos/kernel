e2b7b3dada4db77b81ad04c8062d465b5d447127
Jonathan Voss <jv96@mahurangi.school.nz>
2022-10-30 03:26:21 +1300

    windows: added framerate counter

--- arch/i686/graphics/window.c
+++ arch/i686/graphics/window.c
@@ -4,8 +4,11 @@
 #include "heap.h"
 #include "libio.h"
 #include "mouse.h"
+#include "pit.h"
 
 Window *windows[];
+uint32_t frames = 0;
+uint32_t fps    = 0;
 
 // Get the window at the given coordinates
 Window *getWindowAt() {
@@ -24,20 +27,30 @@ Window *getWindowAt() {
 void wmRedrawWindows() {
     gfxFramebufferClear(gfxColor(0x00, 0x6E, 0x8D));
 
-    if (windows)
-        for (int i = 0; i < (sizeof(Window) / sizeof(windows[0])); i++) {
-            Window *window = windows[i];
-            gfxRestoreTempBuffer(window->x, window->y, window->w, window->h, window->buffer);
-        }
+    // if (windows)
+    //     for (int i = 0; i < (sizeof(Window) / sizeof(windows[0])); i++) {
+    //         Window *window = windows[i];
+    //         gfxRestoreTempBuffer(window->x, window->y, window->w, window->h, window->buffer);
+    //     }
+
+    gfxDrawRect(50, 50, 200, 200, gfxColor(0x00, 0x6E, 0xFF));
 
     // Draw the mouse cursor
     gfxDrawCursor(mouseX, mouseY, gfxColor(0xFF, 0xFF, 0xFF));
+
+    if (timer_ticks % 18 == 0) {
+        fps    = frames;
+        frames = 0;
+    }
+    gfxPutStringAt(10, 10, "FPS: ");
+    gfxPutStringAt(82, 10, itoa(fps, 10));
 
     gfxSwapBuffers();
 }
 
 void initWindowManager(void) {
     for (;;) {
+        frames++;
         wmRedrawWindows();
     }
 }
